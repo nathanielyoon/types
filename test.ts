064b73da0097f542041ae74bcb39a257299d5619
nathanielyoon <nathanielyoon1@gmail.com>
2025-06-12 21:14:02 -0400

    feat: update test

--- test.ts
+++ test.ts
@@ -1,16 +1,48 @@
-import { assertEquals } from "jsr:@std/assert@^1.0.13";
+import { Row } from "jsr:@nyoon/csv@^1.0.9";
+import { assert, assertEquals } from "jsr:@std/assert@^1.0.13";
 import fc from "npm:fast-check@^4.1.1";
+import { Json, Type } from "./mod.ts";
 import * as mod from "./mod.ts";
 
-const b = {
-  opt: [
-    fc.tuple(fc.constant("")),
-    fc.constant(undefined),
-  ],
-} satisfies {
-  [B in keyof Omit<typeof mod, "flag" | "open">]?: {
-    [C in Exclude<keyof Parameters<typeof mod[B]>, keyof []>]: fc.Arbitrary<
-      Parameters<typeof mod[B]>[C]
-    >;
-  };
+const all = {} as {
+  [A in keyof Omit<typeof mod, "flag" | "open">]: [
+    type: Type,
+    ok: Row,
+    no: [Json, Row],
+  ];
 };
+const test = <A extends keyof Omit<typeof mod, "flag" | "open">>(
+  name: A,
+  arbitraries: fc.Arbitrary<Parameters<typeof mod[A]>>,
+  to: (...$: Parameters<typeof mod[A]>) => [Row[], [Json, Row[]][]],
+) =>
+  Deno.test(name, () =>
+    fc.assert(
+      fc.property(arbitraries, ($) => {
+        const a = (mod[name] as (...$: any) => Type)(...$), b = to(...$);
+        b[0].forEach(($) => {
+          const c = a.decode($);
+          assert(typeof c !== "symbol"), assertEquals(a.encode(c), $);
+        });
+        b[1].forEach(([json, rows]) =>
+          rows.forEach(($) => {
+            const c = a.decode($);
+            assert(typeof c === "symbol"), assertEquals(mod.open(c), json);
+          })
+        );
+        all[name] = [a, b[0][0], b[1][0]];
+      }),
+      { numRuns: 16 },
+    ));
+test(
+  "opt",
+  fc.uniqueArray(fc.string(), { minLength: 1 })
+    .map(([$, ...$$]) => [[$, ...$], { optional: !($$.length & 1) }]),
+  (kind, meta) => {
+    const a = [], b = [];
+    if (meta?.optional) {
+      a.push([null]);
+    }
+    throw 0;
+  },
+);
