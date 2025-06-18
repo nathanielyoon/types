e9df66fd000a437e74ffbf570dd1dac7a27f0945
nathanielyoon <nathanielyoon1@gmail.com>
2025-06-18 17:28:21 -0400

    feat: add more tests

--- test.ts
+++ test.ts
@@ -6,6 +6,7 @@ import {
   assertNotEquals,
 } from "jsr:@std/assert@^1.0.13";
 import fc from "npm:fast-check@^4.1.1";
+import { b_s64 } from "@nyoon/base";
 import {
   clamp,
   flag,
@@ -60,34 +61,31 @@ Deno.test("clamp", () =>
       assertLessOrEqual(a, b);
     },
   )));
-const all: [Type, ok: Row[], no: [Row, Json][]][] = [];
 type Falsy = undefined | null | false | 0 | 0n | "";
-const assert_type =
-  (type: Type) => (ok: (Row | Falsy)[], no: ([Row, Json] | Falsy)[]) => {
-    const a = ok.filter(($) => Array.isArray($));
-    for (const row of a) {
-      const b = type.parse([...row]);
-      try {
-        assert(typeof b !== "symbol");
-      } catch ($) {
-        console.log({ type, row, data: b });
-        throw $;
-      }
-      assertEquals(type.stringify(b), row);
+const type = (type: Type, ok: (Row | Falsy)[], no: ([Row, Json] | Falsy)[]) => {
+  const a = ok.filter(($) => Array.isArray($));
+  for (const row of a) {
+    const b = type.parse([...row]);
+    try {
+      assert(typeof b !== "symbol");
+    } catch ($) {
+      console.log({ type, row, data: b });
+      throw $;
     }
-    const b = no.filter(($) => Array.isArray($));
-    for (const [row, json] of b) {
-      const c = type.parse([...row]);
-      try {
-        assert(typeof c === "symbol");
-      } catch ($) {
-        console.log({ type, row, data: c });
-        throw $;
-      }
-      assertEquals(open(c), json);
+    assertEquals(type.stringify(b), row);
+  }
+  const b = no.filter(($) => Array.isArray($));
+  for (const [row, json] of b) {
+    const c = type.parse([...row]);
+    try {
+      assert(typeof c === "symbol");
+    } catch ($) {
+      console.log({ type, row, data: c });
+      throw $;
     }
-    all.push([type, a, b]);
-  };
+    assertEquals(open(c), json);
+  }
+};
 Deno.test("nil", () =>
   [
     opt([""]).really(),
@@ -96,7 +94,7 @@ Deno.test("nil", () =>
     vec("time").maybe(),
     obj({ char: "char" }),
   ].forEach(($, z) =>
-    assert_type($)([z & 1 && [null]], [z & 1 ^ 1 && [[null], "valueMissing"]])
+    type($, [z & 1 && [null]], [z & 1 ^ 1 && [[null], "valueMissing"]])
   ));
 Deno.test("hook", async ($) => {
   const step = <A extends keyof Type["hooks"]>(
@@ -120,80 +118,80 @@ Deno.test("hook", async ($) => {
     fc.boolean(),
     fc.boolean(),
     fc_number(),
-    (one, two, $) =>
-      assert_type(
-        num("real")
-          .on("post_parse", ($) => one ? $ : flag(1))
-          .on("post_parse", ($) => two ? $ : flag(2)),
-      )(
-        [one && two && [`${$}`]],
-        [!one && [[`${$}`], 1] || !two && [[`${$}`], 2]],
-      ),
+    (one, two, $) => {
+      const a = num("real")
+        .on("post_parse", ($) => one ? $ : flag(1))
+        .on("post_parse", ($) => two ? $ : flag(2))
+        .parse([`${$}`]);
+      if (one && two) assertEquals(a, $);
+      else assert(typeof a === "symbol"), assertEquals(open(a), one ? 2 : 1);
+    },
   ));
 });
 Deno.test("opt", () =>
   fc.assert(
     fc.property(fc.uniqueArray(fc.string(), { minLength: 1 }), (kind) =>
-      assert_type(opt(kind as [string, ...string[]]))(
+      type(
+        opt(kind as [string, ...string[]]),
         kind.map(($) => [$]),
         kind.reduce(
-          (no, $) =>
-            kind.includes($ += "!") ? no : [...no, [[$], "badInput"]],
+          (no, $) => kind.includes($ += "!") ? no : [...no, [[$], "badInput"]],
           [] as [Row, Json][],
         ),
       )),
   ));
-Deno.test("num", () =>
-  ([["uint", Math.floor], ["time", Math.floor], ["real", Number]] as const)
-    .forEach(([kind, map]) => {
-      const to = (numbers: number[]) =>
-        numbers.map(($) => {
-          if (kind === "uint") return $.toString(16);
-          if (kind === "time") return $.toString(16).padStart(12, "0");
-          return `${$}`;
-        });
-      assert_type(num(kind))(
-        [to([0])],
-        ["", " ", "!"].map((raw) => [[raw], "badInput"] as const),
-      );
-      fc.assert(fc.property(
-        fc.record({
-          min: fc_number({ min: 0, max: 0xffffffff }).map(map),
-          max: fc_number({ min: 0, max: 0xffffffff }).map(map),
-          step: fc_number({ min: 0, max: 0xff }).map(map),
-        }),
-        (meta) => {
-          assert_type(vec(kind))([["3"].concat(to(Object.values(meta)))], []);
-          const [a, b] = clamp(RANGE.uint, meta);
-          assert_type(num(kind, { min: a }))(
-            [to([a]), a < b && to([a + 1])],
-            [a && [to([a - 1]), "rangeUnderflow"]],
-          );
-          assert_type(num(kind, { max: b }))(
-            [to([b]), b && to([b - 1])],
-            [[to([b + 1]), "rangeOverflow"]],
-          );
-          meta.step > 1 && assert_type(num(kind, { step: meta.step }))(
-            [to([meta.step])],
-            [[to([meta.step + 1]), "stepMismatch"]],
-          );
-        },
-      ));
-    }));
-Deno.test("all", () => {
-  const a: { [key: string]: Type } = {}, b = Array<Row>(all.length);
-  for (let z = 0; z < all.length; ++z) {
-    a[z] = vec(all[z][0]);
-    b[z] = [all[z][1].length.toString(36), ...all[z][1].flat()];
+Deno.test("num", async ($) => {
+  const step = (kind: Parameters<typeof num>[0], arb: fc.Arbitrary<number>) =>
+    $.step(kind, () => {
+      const a = ($: number) =>
+        $.toString(kind === "real" ? 10 : 16)
+          .padStart(kind === "time" ? 12 : 0, "0");
+      type(num(kind), [[a(0)]], [
+        ...["", " ", "!"].map<[Row, Json]>(($) => [[$], "badInput"]),
+        [["00"], "typeMismatch"],
+      ]);
+      fc.assert(fc.property(arb, arb, (min, max) => {
+        type(vec(num(kind)), [["2", a(min), a(max)]], []);
+        const [c, d] = clamp(RANGE[kind], { min, max });
+        type(num(kind, { min: c, max: d }), [
+          [a(c)],
+          [a(d)],
+          c + 1 <= d && [a(c + 1)],
+          d - 1 >= c && [a(d - 1)],
+        ], [
+          c && c - 1 < c && [[a(c - 1)], "rangeUnderflow"],
+          d + 1 > d && [[a(d + 1)], "rangeOverflow"],
+        ]);
+      }));
+      fc.assert(fc.property(arb.map(($) => $ & 0xff || 1), ($) => {
+        type(num(kind), [[a($)]], []);
+        type(num(kind, { step: $ }), [[a($)]], [
+          $ > 1 && [[a($ + 1)], "stepMismatch"],
+        ]);
+        type(num(kind, { step: 0 }), [[a($)]], []);
+      }));
+    });
+  await step("uint", fc_number({ min: 0, max: -1 >>> 0 }).map(Math.floor));
+  await step("time", fc_number({ min: 0, max: 2 ** 48 - 1 }).map(Math.floor));
+  await step("real", fc_number());
+});
+Deno.test("str", async ($) => {
+  await $.step("pkey", () => {
+    fc.assert(fc.property(
+      fc.uint8Array({ minLength: 32, maxLength: 32 }).map(b_s64),
+      fc.oneof(
+        fc.uint8Array({ maxLength: 31 }),
+        fc.uint8Array({ minLength: 33 }),
+      ).map(b_s64),
+      (key, not) => type(str("pkey"), [[key]], [[[not], "badInput"]]),
+    ));
+    fc.assert(fc.property(
+      fc.stringMatching(/[^-\w]|^(?:[-\w]{0,42}|[-\w]{44,})$/),
+      ($) => assertEquals(str("pkey").stringify($), ["A".repeat(43)]),
+    ));
+  });
+  for (const kind of ["char", "text"] as const) {
+    await $.step(kind, () => {
+    });
   }
-  const c = b.length.toString(36);
-  assert_type(obj(a))(
-    [[c, ...b.flat()]],
-    [],
-    // all.flatMap(($, z) =>
-    //   $[2].map(([row, json]) =>
-    //     [[c, ...b.with(z, row).flat()], { [z]: json }] as const
-    //   )
-    // ),
-  );
 });
